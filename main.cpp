670048b14d09cbb53b9882fc8bbefc93d8249be8
hahhhhhhhung123-pixel <hahhhhhhhung123@gmail.com>
2025-10-24 22:51:49 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int factorial(int n) {
-    if (n <= 1) return 1;
-    return n * factorial(n - 1);
-}
-
 int main() {
-    int x = 5;
-    int result = factorial(x);
-    cout << "Factorial of " << x << " is " << result << endl;
+    cout << "Hello world";
+    
     return 0;
 }
